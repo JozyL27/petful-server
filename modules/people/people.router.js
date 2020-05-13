7a16752ee0f017cb033459960a630f4754e5d5e0
Jose <jozylopez27@gmail.com>
2020-05-12 19:32:53 -0500

    people endpoint

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -6,11 +6,18 @@ const People = require('./people.service')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  // Return all the people currently in the queue.
+  res.json(People.get())
 })
 
 router.post('/', json, (req, res) => {
-  // Add a new person to the queue.
+  
+  const { name } = req.body
+  const person = name
+
+  People.enqueue(person)
+
+  res.set(201).json(person)
 })
+
 
 module.exports = router
